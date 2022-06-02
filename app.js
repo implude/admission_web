caea9c8231c20d9f9d7c4f06b922f2079202ff9d
eks05 <eks0220@naver.com>
2022-06-02 20:37:34 +0900

    진짜 마지막 오늘

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("server is running")
 })
