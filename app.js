a2786809171a06f48a40fa8e04a67feefefb1cbc
eks05 <eks0220@naver.com>
2022-06-05 00:10:12 +0900

    끝?

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log("server is running")
 })
--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,18 @@ router
   .get('/input', (req, res) => {
     res.render('input')
   })
+  .post('/result', (req, res) => {
+    console.log(req.body)
+    res.render('result',
+      {
+        name: req.body.name,
+        p1: req.body.p1,
+        p2: req.body.p2,
+        p3: req.body.p3,
+        email: req.body.email,
+        address: req.body.address,
+        circle: req.body.circle
+      })
+  })
 
 module.exports = router;
